55735b33969e980e15bd2ab1223d8f73c0cb5b94
XPC <XPC@DESKTOP-FO0EKAT>
2022-04-19 21:19:52 -0600

    II Examen Alejandro Gonzalez Quesada

---
+++ src/main/java/com/examen/Examen_II/ExamenIiApplication.java
@@ -0,0 +1,13 @@
+package com.examen.Examen_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/Examen_II/ExamenIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.Examen_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
